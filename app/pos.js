f1b13026f23b121fc18a5fdcba16845c0d12c34b
mike.lima <mike.lima@tegra.me>
2016-12-10 21:52:37 -0200

    Fix bug

--- app/pos.js
+++ app/pos.js
@@ -2,9 +2,9 @@ module.exports = function (app) {
 
     app.post('/api/pos/venda', function (req, res) {
 
-        console.log("POS INICIO");
+        console.log("POS Inicio Venda");
         console.log(req.body);
-        console.log("POS VENDA");
+        console.log("POS Venda");
 
         res.status(200).send();
     });
