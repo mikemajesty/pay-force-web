e73021984b4251ba746379ccc5d41d511042a1e7
mike.lima <mike.lima@tegra.me>
2016-12-10 21:48:57 -0200

    Fix bug

--- app/mobile.js
+++ app/mobile.js
@@ -1,15 +1,6 @@
 module.exports = function (app) {
 
-    app.post('/api/mobile/venda', function(req, res) {
-
-        console.log("Post Inicio Venda");
-        console.log(req.body);
-        console.log("Post Venda");
-
-        res.status(200).send();
-    });
-
-     app.post('/api/mobile/confirmar', function(req, res) {
+    app.post('/api/mobile/confirmar', function (req, res) {
 
         console.log("Post Inicio Confirmar");
         console.log(req.body);
@@ -18,7 +9,7 @@ module.exports = function (app) {
         res.status(200).send();
     });
 
-     app.post('/api/mobile/negacao', function(req, res) {
+    app.post('/api/mobile/negacao', function (req, res) {
 
         console.log("Post Inicio Negação");
         console.log(req.body);
--- app/pos.js
+++ app/pos.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
 
-    app.post('/api/pos/venda', function(req, res) {
+    app.post('/api/pos/venda', function (req, res) {
 
         console.log("POS INICIO");
         console.log(req.body);
