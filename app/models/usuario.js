409422d18f4657d73be4750aef7427edc43ce41f
Celso Wo <celso.wo@tegra.me>
2016-12-10 21:52:19 -0200

    Added registrado view

--- app/models/usuario.js
+++ app/models/usuario.js
@@ -1,10 +1,11 @@
 var mongoose = require('mongoose');
 var Movimento = require('./movimento');
-var Cartao = require('./cartao');
+var Conta = require('./conta');
 
 module.exports = mongoose.model('User', {
-    numero: {type: String, default: ''},
+    nome: {type: String, default: ''},
+    telefone: {type: String, default: ''},
     valor: {type: Number, default: 0},
     movimentos: [Movimento],
-    cartoes: [Cartao]
+    contas: [Conta]
 });
