afccffba35f46941fb7eb210cc3b3bd209cdf7e9
mike.lima <mike.lima@tegra.me>
2016-12-10 15:09:46 -0200

    Add charts

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,7 +1,7 @@
 angular.module('todoController', [])
 
 	// inject the Todo service factory into our controller
-	.controller('mainController', ['$scope','$http','Todos', function($scope, $http, Todos) {
+	.controller('mainController', ['$scope', '$http', 'Todos', function ($scope, $http, Todos) {
 		$scope.formData = {};
 		$scope.loading = true;
 
@@ -9,14 +9,14 @@ angular.module('todoController', [])
 		// when landing on the page, get all todos and show them
 		// use the service to get all the todos
 		Todos.get()
-			.success(function(data) {
+			.success(function (data) {
 				$scope.todos = data;
 				$scope.loading = false;
 			});
 
 		// CREATE ==================================================================
 		// when submitting the add form, send the text to the node API
-		$scope.createTodo = function() {
+		$scope.createTodo = function () {
 
 			// validate the formData to make sure that something is there
 			// if form is empty, nothing will happen
@@ -27,7 +27,7 @@ angular.module('todoController', [])
 				Todos.create($scope.formData)
 
 					// if successful creation, call our get function to get all the new todos
-					.success(function(data) {
+					.success(function (data) {
 						$scope.loading = false;
 						$scope.formData = {}; // clear the form so our user is ready to enter another
 						$scope.todos = data; // assign our new list of todos
@@ -35,14 +35,95 @@ angular.module('todoController', [])
 			}
 		};
 
+		var salesData = {
+			labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+			datasets: [
+				{
+					label: "Front",
+					fillColor: "rgba(247, 80, 90, 0.0)",
+					strokeColor: "#F7505A",
+					pointColor: "#F7505A",
+					pointStrokeColor: "rgba(0,0,0,0.2)",
+					pointHighlightStroke: "rgba(225,225,225,0.75)",
+					data: [3400, 3400, 3400, 3400, 3400, 3400, 3400]
+				},
+				{
+					label: "Middle",
+					fillColor: "rgba(255, 172, 100, 0.0)",
+					strokeColor: "rgba(255, 172, 100, 1)",
+					pointColor: "rgba(255, 172, 100, 1)",
+					pointStrokeColor: "rgba(0,0,0,0.2)",
+					pointHighlightStroke: "rgba(225,225,225,0.75)",
+					data: [3400, 3400, 3400, 3400, 3200, 3200, 3200]
+				}
+			]
+		};
+		var ctx = document.getElementById("salesData").getContext("2d");
+		window.myLineChart = new Chart(ctx).Line(salesData, {
+			pointDotRadius: 6,
+			pointDotStrokeWidth: 2,
+			datasetStrokeWidth: 3,
+			scaleShowVerticalLines: false,
+			scaleGridLineWidth: 2,
+			scaleShowGridLines: true,
+			scaleGridLineColor: "rgba(225, 255, 255, 0.015)",
+			scaleOverride: true,
+			scaleSteps: 9,
+			scaleStepWidth: 500,
+			scaleStartValue: 0,
+
+			responsive: true
+
+		});
+
+		var creditSales = new ProgressBar.Circle('#creditSales', {
+			color: '#F7505A',
+			strokeWidth: 5,
+			trailWidth: 3,
+			duration: 1000,
+			text: {
+				value: '0%'
+			},
+			step: function (state, bar) {
+				bar.setText((bar.value() * 100).toFixed(0) + "%");
+			}
+		});
+		var channelSales = new ProgressBar.Circle('#channelSales', {
+			color: '#e88e3c',
+			strokeWidth: 5,
+			trailWidth: 3,
+			duration: 1000,
+			text: {
+				value: '0%'
+			},
+			step: function (state, bar) {
+				bar.setText((bar.value() * 100).toFixed(0) + "%");
+			}
+		});
+		var directSales = new ProgressBar.Circle('#directSales', {
+			color: '#2bab51',
+			strokeWidth: 5,
+			trailWidth: 3,
+			duration: 1000,
+			text: {
+				value: '0%'
+			},
+			step: function (state, bar) {
+				bar.setText((bar.value() * 100).toFixed(0) + "%");
+			}
+		});
+		creditSales.animate(0.8);
+		channelSales.animate(0.64);
+		directSales.animate(0.34);
+
 		// DELETE ==================================================================
 		// delete a todo after checking it
-		$scope.deleteTodo = function(id) {
+		$scope.deleteTodo = function (id) {
 			$scope.loading = true;
 
 			Todos.delete(id)
 				// if successful creation, call our get function to get all the new todos
-				.success(function(data) {
+				.success(function (data) {
 					$scope.loading = false;
 					$scope.todos = data; // assign our new list of todos
 				});
