8c491d94820de9dae13ecba03caec5acec96cb23
mike.lima <mike.lima@tegra.me>
2016-12-11 02:26:58 -0200

    fix bug

--- app/models/transacao.js
+++ app/models/transacao.js
@@ -3,5 +3,5 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('Transacao', {
     telefone: {type: String, default: ''},
     valor: {type: Number, default: ''},
-    status: {type: String, default: null},
+    status: {type: String, default: undefined},
 });
