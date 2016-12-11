398d7c5ebecadb9fd6daafced02ec0ffe8a35cee
mike.lima <mike.lima@tegra.me>
2016-12-11 02:24:34 -0200

    fix bug

--- app/pos.js
+++ app/pos.js
@@ -16,7 +16,7 @@ module.exports = function(app) {
         Transacao.findById(req.query.id, function(err, trans) {
             if (trans == null)
                 res.send("Gabriel Safado");
-            if(trans.status)
+            else if(trans.status)
                 res.send({status: trans.status});
             else
                 res.send(400);
