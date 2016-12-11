0dba9f6af3f94f5faadba276f597e0d68fabc71c
mike.lima <mike.lima@tegra.me>
2016-12-11 03:50:23 -0200

    Add get user

--- app/mobile.js
+++ app/mobile.js
@@ -1,17 +1,25 @@
 var Usuario = require('./models/usuario.js');
+var Transacao = require('./models/usuario.js');
 
 module.exports = function(app) {
 
     app.get('/api/mobile/usuario', function(req, res) {
         Usuario.findOne({ 'telefone': req.query.telefone }, function(err, user) {
             if (!err) {
-                console.log('user', user);
-                user.status = 'S';
-                user.visits.$inc();
-                user.save();
-                res.send(user);
+              res.send(user);
             }
             res.status(400);
+        });
+    });
+
+    app.get('/api/mobile/confirma', function(req, res) {
+        Transacao.findOne({ 'telefone': req.query.telefone }, function(err, tras) {
+            if (!err) {
+              tras.status = "S";
+              tras.save();
+              res.send(user);
+            }
+            res.status(tras);
         });
     });
 };
