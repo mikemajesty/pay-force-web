c79d1f3c6051f6cf726367a7e1bdf0c461cc4fba
mike.lima <mike.lima@tegra.me>
2016-12-10 22:30:39 -0200

    fix get user

--- app/mobile.js
+++ app/mobile.js
@@ -3,7 +3,6 @@ var Usuario = require('./models/usuario.js');
 module.exports = function (app) {
 
     app.get('/api/mobile/usuario', function (req, res) {
-        console.log('req.query.telefone', req.query.telefone);
         Usuario.findOne({ 'telefone': req.query.telefone }, function (err, user) {
             if (user) {
                 res.status(user).send();
