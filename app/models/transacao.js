df3011b964ef80c9349418b6240caa418e1f4e4b
mike.lima <mike.lima@tegra.me>
2016-12-11 00:26:19 -0200

    Add transacao model

---
+++ app/models/transacao.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('Transacao', {
+    telefone: {type: String, default: ''},
+    valor: {type: Number, default: ''},
+    status: {type: String, default: null},
+});
--- app/pos.js
+++ app/pos.js
@@ -1,19 +1,23 @@
 var Usuario = require('./models/usuario.js');
+var Transacao = require('./models/transacao.js');
+module.exports = function(app) {
 
-module.exports = function (app) {
+    app.post('/api/pos/venda', function(req, res) {
 
-    app.post('/api/pos/venda', function (req, res) {
-
-        Usuario.findOne({ 'telefone': req.body.telefone }, function (err, user) {
-            if (user) {
-                res.status(200).send();
-            }
-            res.status(200).send();
-        });
-
+        var transacao = new Transacao({ telefone: req.body.telefone, valor: req.body.valor });
+        transacao.save();
+        res.send(transacao._id);
     });
 
-    app.post('/api/pos/resultado', function (req, res) {
-       res.status(400).json({confirmado: "OK"});
+    app.post('/api/pos/resultado', function(req, res) {
+        res.status(400).json({ confirmado: "OK" });
     });
 };
+
+// Usuario.findOne({ 'telefone': req.body.telefone }, function (err, user) {
+//     if (user) {
+//         res.status(200).send();
+//     }
+//     res.status(200).send();
+// });
+
