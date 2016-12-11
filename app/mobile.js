6a0f1506712179369735c17198fd78f486b13564
mike.lima <mike.lima@tegra.me>
2016-12-11 03:13:47 -0200

    fix bug

--- app/mobile.js
+++ app/mobile.js
@@ -3,20 +3,14 @@ var Usuario = require('./models/usuario.js');
 module.exports = function(app) {
 
     app.get('/api/mobile/usuario', function(req, res) {
-        // Usuario.findOne({ telefone: req.query.telefone }, function (err, user) {
-        //     if (user) {
-        //         res.status(user).send();
-        //     }
-        //     res.status(400).send(user);
-        // });
-        Usuario.find({}, function(err, users) {
-            var userMap = {};
-
-            users.forEach(function(user) {
-                userMap[user._id] = user;
-            });
-
-            res.send(userMap);
+        Usuario.findOne({ telefone: req.query.telefone }, function(err, user) {
+            if (user) {
+                res.send(user);
+                doc.name = 'jason borne';
+                doc.visits.$inc();
+                doc.save();
+            }
+            res.status(400).send(user);
         });
     });
 };
